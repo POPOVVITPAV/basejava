0bf3cbdc7c3d8856142fb43ba3973bcfccff2185
Vitaly <POPOVVITPAV@yandex.ru>
2023-01-19 16:04:28 +0300

    lesson 2 end

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,22 +5,22 @@ import java.util.Objects;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    private int size;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+   public void save(Resume r) {
         if (size < storage.length) {
             storage[size] = r;
             size++;
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (Objects.equals(storage[i].uuid, uuid)) {
                 return storage[i];
@@ -43,7 +43,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
